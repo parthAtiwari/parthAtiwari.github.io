23a44dc724bb058f1caef657f1caf53574778074
parthAtiwari <95771107+parthAtiwari@users.noreply.github.com>
2022-09-11 14:35:35 +0530

    Delete extract.js

--- scripts/extract.js
+++
@@ -1,4 +0,0 @@
-let file = "https://leetcode-stats-api.herokuapp.com/parthtiwari"
-fetch (file)
-.then(x => x.text())
-.then(y => document.getElementById("leetcode").innerHTML = y.substring(56,59));
