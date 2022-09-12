7f6ec5baa003a94de2cbb0d77ae7d4a95deea35d
parthAtiwari <95771107+parthAtiwari@users.noreply.github.com>
2022-09-12 11:28:27 +0530

    Update extract.js

--- scripts/extract.js
+++ scripts/extract.js
@@ -1,4 +1,4 @@
 let file = "https://leetcode-stats-api.herokuapp.com/parthtiwari"
 fetch (file)
-.then(x => x.text())
-.then(y => document.getElementById("leetcode").innerHTML = y.substring(56,59));
+.then(x =>x.text())
+.then(y => document.getElementById("leetcode").innerHTML = y.split(",")[2].split(':')[1]);
